4fa3898decee41b00723bb86e2b7375a0a587bee
Z4NTxkub <139094273+Z4NTxkub@users.noreply.github.com>
2023-07-17 10:08:06 +0700

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{	string Name;
+	short Year;
+	cout << "Enter name : ";
+	cin >> Name;
+	cout << "Enter year of birth : ";
+	cin >> Year;
+	cout << "\nYour Age" <<2566- Year << "year old. " <<endl;
+	system ("pause");
+	return(0);
+
+
+
+}
